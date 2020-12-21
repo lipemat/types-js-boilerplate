93bd79f0765cb56999c9809600ef9ceeb33ebd63
lipemat <mat@matlipe.com>
2020-12-21 12:56:37 -0700

    Include the remaining @types in index file

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,3 @@
-
 // WordPress REST API
 import './wordpress__api/Categories';
 import './wordpress__api/Comments';
@@ -10,6 +9,21 @@ import './wordpress__api/Taxonomies';
 import './wordpress__api/Types';
 import './wordpress__api/Users';
 import './wordpress__api/index';
+
+import './wordpress__api-fetch/index';
+import './wordpress__block-editor/index';
+import './wordpress__blocks/index';
+import './wordpress__components/index';
+import './wordpress__compose/index';
+import './wordpress__core-data/index';
+import './wordpress__data/index';
+import './wordpress__editor/index';
+import './wordpress__element/index';
+import './wordpress__hooks/index';
+import './wordpress__i18n/index';
+import './wordpress__icons/index';
+import './wordpress__primitives/index';
+import './wordpress__url/index';
 
 // PostCSS modules.
 import './pcss/index';
