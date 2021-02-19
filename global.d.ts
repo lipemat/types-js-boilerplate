7df79f68c2e2fe42c2ed724777e7956577afe2e3
lipemat <mat@matlipe.com>
2021-02-19 13:28:45 -0500

    Bump to 1.1.4

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,12 @@
 import {WpMedia} from '@lipemat/js-boilerplate/global/wp-media';
 
+/**
+ * Adds the global `wp` variable to `window` definitions.
+ *
+ * If additional declarations on the `wp` variable are needed
+ * simply copy/paste this block into your project and define
+ * additional properties.
+ */
 declare global {
 	interface Window {
 		wp: {
