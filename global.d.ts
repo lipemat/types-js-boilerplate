451a7f1a746de32f3710825ff88862b68b8d7003
lipemat <mat@matlipe.com>
2022-02-01 19:48:42 -0500

    Declare all available `wp` packages
    Anything we have a definition for, we add to the global `wp` definition,
    so we can use them directly as needed.

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,21 @@
 import {WpMedia} from '@lipemat/js-boilerplate/global/wp-media';
 import {WpCodeEditor} from '@lipemat/js-boilerplate/global/wp-code-editor';
+import ApiFetch from '@wordpress/api-fetch';
+import EditPost from '@wordpress/edit-post';
+import BlockEditor from '@wordpress/block-editor';
+import Blocks from '@wordpress/blocks';
+import Components from '@wordpress/components';
+import CoreData from '@wordpress/core-data';
+import Data from '@wordpress/data';
+import Editor from '@wordpress/editor';
+import Element from '@wordpress/element';
+import {Hook} from '@wordpress/hooks';
+import I18N from '@wordpress/i18n';
+import Icons from '@wordpress/icons';
+import MediaUtils from '@wordpress/media-utils';
+import Plugins from '@wordpress/plugins';
+import Primitives from '@wordpress/primitives';
+import URL from '@wordpress/url';
 
 /**
  * Adds the global `wp` variable to `window` typescript definitions.
@@ -17,8 +33,24 @@ import {WpCodeEditor} from '@lipemat/js-boilerplate/global/wp-code-editor';
 declare global {
 	interface Window {
 		wp: {
-			media: WpMedia;
-			codeEditor: WpCodeEditor;
+			apiFetch?: typeof ApiFetch,
+			blockEditor?: BlockEditor;
+			blocks?: Blocks,
+			codeEditor?: WpCodeEditor;
+			components?: Components;
+			coreData?: CoreData;
+			data?: Data;
+			editPost?: EditPost,
+			editor?: Editor;
+			element?: Element;
+			hooks?: Hook;
+			i18n?: I18N;
+			icons?: Icons;
+			media?: WpMedia;
+			mediaUtils?: MediaUtils;
+			plugins?: Plugins;
+			primitives?: Primitives;
+			url?: URL;
 		}
 	}
 }
