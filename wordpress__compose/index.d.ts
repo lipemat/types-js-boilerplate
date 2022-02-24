7b052638e6ec95b40db50829e21f977345b7bc2d
lipemat <mat@matlipe.com>
2022-02-24 12:47:59 -0500

    Declare `useCopyToClipboard` from `@wordpress/compose`
    @example
    
    ```ts
    	const copyRef = useCopyToClipboard( serialize( getBlocksByClientId( [ clientId ] ) ), () => {
    		createSuccessNotice( 'Copied Stripe to clipboard', {
    			isDismissible: true,
    			type: 'snackbar',
    		} );
    	} );
    <button ref={copy}>
    					Copy
    				</button>
    
    ```

--- wordpress__compose/index.d.ts
+++ wordpress__compose/index.d.ts
@@ -1,22 +1,35 @@
 declare module '@wordpress/compose' {
-	import {ComponentType} from 'react';
+	import {ComponentType, MutableRefObject} from 'react';
 
 	// @note displayName is assigned directly to component, not a prop.
 	export interface createHigherOrderComponentProps {
 		displayName?: string;
 	}
+
 	type createHigherOrderComponent = <P>( WrappedComponent: ComponentType<P> ) => ComponentType<P> & createHigherOrderComponentProps;
+
+	/**
+	 * Copy text to clipboard.
+	 *
+	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/#usecopytoclipboard
+	 */
+	type useCopyToClipboard = ( text: string | ( () => string ), onSuccess: () => void ) => MutableRefObject<string>;
+
 
 	export interface withInstanceIdProps {
 		instanceId?: string;
 	}
+
 	type withInstanceId = <P>( WrappedComponent: ComponentType<P> ) => ComponentType<P & withInstanceIdProps> & createHigherOrderComponentProps;
 
+
 	export const createHigherOrderComponent: createHigherOrderComponent;
+	export const useCopyToClipboard: useCopyToClipboard;
 	export const withInstanceId: withInstanceId;
 
 	export default interface Compose {
 		createHigherOrderComponent: createHigherOrderComponent;
+		useCopyToClipboard: useCopyToClipboard;
 		withInstanceId: withInstanceId;
 	}
 }
