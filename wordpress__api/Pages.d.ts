4ecc89c2fb8700a162fed68eb9bd0e573191d6c3
lipemat <mat@matlipe.com>
2022-08-05 19:16:14 -0400

    Fix issues with 'page' entities
    Turns out the system requires the 'id' field to be available in the
    interface without inferring it from another.
    
    Added all the REST types to Pages which fixed the issues.

--- wordpress__api/Pages.d.ts
+++ wordpress__api/Pages.d.ts
@@ -7,18 +7,62 @@
  *
  */
 declare module '@wordpress/api/pages' {
-	import {Context, Global} from '@wordpress/api';
-	import {Post, PostCreate, PostsQuery} from '@wordpress/api/posts';
+	import {
+		CommentingStatus,
+		Context,
+		ContextualField,
+		Global,
+		Links,
+		PingStatus,
+		RenderedText
+	} from '@wordpress/api';
+	import {PostCreate, PostMeta, PostsQuery, PostStatus} from '@wordpress/api/posts';
 	import {OmitNever} from '@lipemat/js-boilerplate/utility';
-
+	import {User} from '@wordpress/api/users';
+	import {Media} from '@wordpress/api/media';
+	import {Category} from '@wordpress/api/categories';
 	/**
 	 * Pages Schema.
 	 *
 	 * @link https://developer.wordpress.org/rest-api/reference/pages/#schema
 	 */
-	export type Page<C extends Context = 'view'> = OmitNever<Omit<Post<C>, 'categories' | 'tags' | 'format'> & {
+	export type Page<C extends Context = 'view'> = OmitNever<{
 		parent: number;
 		menu_order: number;
+		author: number;
+		comment_status: ContextualField<CommentingStatus, 'view' | 'edit', C>;
+		content: ContextualField<RenderedText<C> & {
+			block_version: ContextualField<string, 'edit', C>;
+			protected: boolean;
+		}, 'view' | 'edit', C>;
+		date: string | null;
+		date_gmt: ContextualField<string | null, 'view' | 'edit', C>;
+		excerpt: RenderedText<C> & {
+			protected: boolean;
+		};
+		featured_media: number;
+		generated_slug: ContextualField<string, 'edit', C>;
+		guid: ContextualField<RenderedText<C>, 'view' | 'edit', C>;
+		id: number;
+		link: string;
+		meta: ContextualField<PostMeta, 'view' | 'edit', C>;
+		modified: ContextualField<string, 'view' | 'edit', C>;
+		modified_gmt: ContextualField<string, 'view' | 'edit', C>;
+		password: ContextualField<string, 'edit', C>;
+		permalink_template: ContextualField<string, 'edit', C>;
+		ping_status: ContextualField<PingStatus, 'view' | 'edit', C>;
+		slug: string;
+		status: ContextualField<PostStatus, 'view' | 'edit', C>;
+		sticky: ContextualField<boolean, 'view' | 'edit', C>;
+		template: ContextualField<string, 'view' | 'edit', C>;
+		title: RenderedText<C>;
+		type: 'post' | string;
+		_links: ContextualField<Links, 'view', C>;
+		_embedded?: ContextualField<{
+			author?: User[];
+			'wp:featuredmedia'?: Media[];
+			'wp:term'?: Category[];
+		}, 'view', C>;
 	}>
 
 
