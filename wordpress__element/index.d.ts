4dece16acac2a24fc9649889ddbc52e53ba4b92e
lipemat <mat@matlipe.com>
2022-12-20 15:31:03 -0500

    Add `render` function to @wordpress/element definition

--- wordpress__element/index.d.ts
+++ wordpress__element/index.d.ts
@@ -5,13 +5,16 @@
  */
 declare module '@wordpress/element' {
 	import {ComponentType, useEffect as reactUseEffect, useState as reactUseState} from 'react';
+	import {render as reactRender} from 'react-dom';
 
 	export const Fragment: ComponentType<{}>;
+	export const render: typeof reactRender;
 	export const useEffect: typeof reactUseEffect;
 	export const useState: typeof reactUseState;
 
 	export default interface Element {
 		Fragment: typeof Fragment;
+		render: typeof render;
 		useEffect: typeof useEffect;
 		useState: typeof useState;
 	}
