c067002d24a6705ed1874661309f12a69f9ad763
lipemat <mat@matlipe.com>
2023-06-13 17:57:24 -0500

    Deprecate the uses of @wordpress/element
    Breaks HMR and React Fast Refresh if don't use your packages version
    instead of the wp global.

--- wordpress__element/index.d.ts
+++ wordpress__element/index.d.ts
@@ -1,6 +1,8 @@
 /**
  * Element is simply, an abstraction layer atop React.
  *
+ * @note You probably don't need to use this.
+ *
  * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-element/
  */
 declare module '@wordpress/element' {
@@ -9,9 +11,20 @@ declare module '@wordpress/element' {
 	import {render as reactRender} from 'react-dom';
 
 	export const Fragment: ComponentType<{}>;
-	export const render: typeof reactRender;
+
 	export const useEffect: typeof reactUseEffect;
+
 	export const useState: typeof reactUseState;
+
+	/**
+	 * @notice This will likely break HMR if you are using React Hot Reloader.
+	 *         It is recommended to use the standard `ReactDOM.render` to allow
+	 *         your `externals` configuration to differentiate between the
+	 *         proper `react-dom` to use.
+	 *
+	 * @deprecated
+	 */
+	export const render: typeof reactRender;
 
 	export default interface Element {
 		Fragment: typeof Fragment;
