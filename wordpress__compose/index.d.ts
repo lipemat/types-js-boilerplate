8deb126de7db6103494bae2f61af3ceb68e0b09c
lipemat <mat@matlipe.com>
2023-09-08 11:19:23 -0500

    Add definitions for the `useDebounce` in @wordpress/compose

--- wordpress__compose/index.d.ts
+++ wordpress__compose/index.d.ts
@@ -8,10 +8,36 @@ declare module '@wordpress/compose' {
 	import {ComponentType} from '@lipemat/js-boilerplate/helpers';
 	import {Ref} from 'react';
 
+
+	type DebounceOptions = {
+		leading: boolean;
+		maxWait: number;
+		trailing: boolean;
+	}
+
+	/**
+	 * @link https://github.com/WordPress/gutenberg/blob/trunk/packages/compose/src/utils/debounce/index.ts
+	 */
+	interface DebouncedFunc<T extends ( ...args: any[] ) => any> {
+		( ...args: Parameters<T> ): ReturnType<T> | undefined;
+		/**
+		 * Throw away any pending invocation of the debounced function.
+		 */
+		cancel(): void;
+		/**
+		 * If there is a pending invocation of the debounced function, invoke it immediately and return its return value.
+		 *
+		 * Otherwise, return the value from the last invocation, or undefined if the debounced function was never invoked.
+		 */
+		flush(): ReturnType<T> | undefined;
+	}
+
+
 	// @note displayName is assigned directly to component, not a prop.
 	export interface createHigherOrderComponentProps {
 		displayName?: string;
 	}
+
 
 	/**
 	 * Given a function mapping a component to an enhanced component and modifier
@@ -29,6 +55,15 @@ declare module '@wordpress/compose' {
 	 */
 	type useCopyToClipboard = <T = HTMLButtonElement>( text: string | (() => string), onSuccess: () => void ) => Ref<T>;
 
+	/**
+	 * Debounce a function like Lodash `debounce`. A new debounced function will
+	 * be returned and any scheduled calls cancelled if any of the arguments change,
+	 * including the function to debounce, so please wrap functions created on
+	 * render in components in `useCallback`.
+	 *
+	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/#usedebounce
+	 */
+	type useDebounce = <T extends (...args: any[]) => any>( func: T, wait: number, options?: DebounceOptions ) => DebouncedFunc<T>;
 
 	export interface withInstanceIdProps {
 		instanceId?: string;
@@ -39,11 +74,13 @@ declare module '@wordpress/compose' {
 
 	export const createHigherOrderComponent: createHigherOrderComponent;
 	export const useCopyToClipboard: useCopyToClipboard;
+	export const useDebounce: useDebounce;
 	export const withInstanceId: withInstanceId;
 
 	export default interface Compose {
 		createHigherOrderComponent: createHigherOrderComponent;
 		useCopyToClipboard: useCopyToClipboard;
+		useDebounce: useDebounce;
 		withInstanceId: withInstanceId;
 	}
 }
