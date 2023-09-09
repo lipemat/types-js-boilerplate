bb3ea5eccdf493a0b6e5424230251aca40bb6fd8
lipemat <mat@matlipe.com>
2023-09-09 14:13:54 -0500

    Add generic for SearchQuery subType

--- wordpress__api/Search.d.ts
+++ wordpress__api/Search.d.ts
@@ -7,6 +7,8 @@
 declare module '@wordpress/api/search' {
 	import {Category, Global, Links, Post} from '@wordpress/api';
 
+	export type SubType = 'post' | 'page' | 'category' | 'post_tag' | 'any';
+
 	/**
 	 * @link https://developer.wordpress.org/rest-api/reference/search-results/#schema
 	 */
@@ -29,14 +31,14 @@ declare module '@wordpress/api/search' {
 	 *
 	 * @see https://make.wordpress.org/core/2022/10/11/miscellaneous-rest-api-improvements-in-wordpress-6-1/
 	 */
-	export interface SearchQuery extends Omit<Global<SearchItem>, '_embed' | 'context'> {
+	export interface SearchQuery<ST extends string = SubType> extends Omit<Global<SearchItem>, '_embed' | 'context'> {
 		context?: 'view' | 'embed';
 		exclude?: number[];
 		include?: number[];
 		page?: number;
 		per_page?: number;
 		search?: string;
-		subtype?: 'post' | 'page' | 'category' | 'post_tag' | 'any';
+		subtype?: ST;
 		type?: 'post' | 'term' | 'post-format';
 		_embed?: 'self' | true;
 	}
