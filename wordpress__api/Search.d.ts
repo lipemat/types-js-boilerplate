fcd48c8e14bab911cb0cfb7b6d8c4217a557faa6
lipemat <mat@matlipe.com>
2023-09-21 18:05:06 -0500

    Separate the return types from the search endpoint
    Receive the differences based on type.

--- wordpress__api/Search.d.ts
+++ wordpress__api/Search.d.ts
@@ -1,26 +1,48 @@
-/* eslint-disable camelcase */
 /**
  * Search REST Endpoint
  *
  * @link https://developer.wordpress.org/rest-api/reference/search-results/
  */
 declare module '@wordpress/api/search' {
-	import {Category, Global, Links, Post} from '@wordpress/api';
+	import {Category, Collection, Global, Links, Post} from '@wordpress/api';
 
 	export type SubType = 'post' | 'page' | 'category' | 'post_tag' | 'any';
 
 	/**
 	 * @link https://developer.wordpress.org/rest-api/reference/search-results/#schema
 	 */
-	export interface SearchItem<ST extends string = Exclude<SubType, 'any'>> {
+	export type SearchItem = PostItem | TermItem | PostFormatItem;
+
+	export interface PostItem<ST extends string = 'post' | 'page'> {
 		id: string | number;
 		title: string;
 		url: string;
-		type: 'post' | 'term' | 'post-format';
+		type: 'post';
 		subtype: ST;
 		_links: Links;
 		_embedded?: {
-			self: Array<Post | Category>
+			self: [ Post | Category ]
+		};
+	}
+
+	export interface TermItem<T extends string = 'category' | 'post_tag'> {
+		id: number;
+		title: string;
+		url: string;
+		type: T;
+		_links: Links;
+		_embedded?: {
+			self: [ Category ]
+		};
+	}
+
+	export interface PostFormatItem {
+		id: string;
+		title: string;
+		url: string;
+		type: 'post-format';
+		_links: {
+			collection: Collection[];
 		};
 	}
 
@@ -31,7 +53,7 @@ declare module '@wordpress/api/search' {
 	 *
 	 * @see https://make.wordpress.org/core/2022/10/11/miscellaneous-rest-api-improvements-in-wordpress-6-1/
 	 */
-	export interface SearchQuery<ST extends string = SubType> extends Omit<Global<SearchItem<ST>>, '_embed' | 'context'> {
+	export interface SearchQuery<ST extends string = SubType> extends Omit<Global<SearchItem>, '_embed' | 'context'> {
 		context?: 'view' | 'embed';
 		exclude?: number[];
 		include?: number[];
