e02729aca291a2a0a434f662c4269e299daed308
lipemat <mat@matlipe.com>
2023-10-23 12:41:25 -0500

    Define a few more types on the wordpress_compose package

--- wordpress__compose/index.d.ts
+++ wordpress__compose/index.d.ts
@@ -2,7 +2,6 @@
  * Definitions for the `@wordpress/compose` package.
  *
  * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/
- * @link https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/wordpress__compose/index.d.ts
  */
 declare module '@wordpress/compose' {
 	import {ComponentType} from '@lipemat/js-boilerplate/helpers';
@@ -16,14 +15,23 @@ declare module '@wordpress/compose' {
 	}
 
 	/**
+	 * Composes multiple higher-order components into a single higher-order component.
+	 *
+	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/#compose
+	 */
+	export function compose( ...funcs: Function[] | Array<Function[]> ): ( ...args: unknown[] ) => any;
+
+	/**
 	 * @link https://github.com/WordPress/gutenberg/blob/trunk/packages/compose/src/utils/debounce/index.ts
 	 */
 	interface DebouncedFunc<T extends ( ...args: any[] ) => any> {
 		( ...args: Parameters<T> ): ReturnType<T> | undefined;
+
 		/**
 		 * Throw away any pending invocation of the debounced function.
 		 */
 		cancel(): void;
+
 		/**
 		 * If there is a pending invocation of the debounced function, invoke it immediately and return its return value.
 		 *
@@ -45,15 +53,33 @@ declare module '@wordpress/compose' {
 	 *
 	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/#createhigherordercomponent
 	 */
-	type createHigherOrderComponent = <T extends ComponentType<any>,
-		R extends ComponentType<any>>( mapCallback: ( WrappedComponent: T) => R, modifierName: string ) => ( Inner: T ) => R & createHigherOrderComponentProps;
+	export function createHigherOrderComponent<
+		T extends ComponentType<any>,
+		R extends ComponentType<any>
+	>( mapCallback: ( WrappedComponent: T ) => R, modifierName: string ): ( Inner: T ) => R & createHigherOrderComponentProps;
+
+	/**
+	 * A simplified and properly typed version of lodash’s debounce, that always uses timers instead of sometimes using rAF.
+	 *
+	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/#debounce
+	 */
+	export function debounce<T extends ( ...args: any[] ) => any>( func: T, wait: number, options?: DebounceOptions ): DebouncedFunc<T>;
+
+	/**
+	 * Higher-order component creator, creating a new component which renders if the given condition is satisfied or with the given optional prop name.
+	 *
+	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/#ifcondition
+	 */
+	export function ifCondition<Props extends {}, R extends ComponentType<Props>>(
+		predicate: ( props: Props ) => boolean
+	): ( WrappedComponent: R ) => R & createHigherOrderComponentProps;
 
 	/**
 	 * Copy text to clipboard.
 	 *
 	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/#usecopytoclipboard
 	 */
-	type useCopyToClipboard = <T = HTMLButtonElement>( text: string | (() => string), onSuccess: () => void ) => Ref<T>;
+	type useCopyToClipboard = <T = HTMLButtonElement>( text: string | ( () => string ), onSuccess: () => void ) => Ref<T>;
 
 	/**
 	 * Debounce a function like Lodash `debounce`. A new debounced function will
@@ -63,7 +89,7 @@ declare module '@wordpress/compose' {
 	 *
 	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-compose/#usedebounce
 	 */
-	type useDebounce = <T extends (...args: any[]) => any>( func: T, wait: number, options?: DebounceOptions ) => DebouncedFunc<T>;
+	type useDebounce = <T extends ( ...args: any[] ) => any>( func: T, wait: number, options?: DebounceOptions ) => DebouncedFunc<T>;
 
 	export interface withInstanceIdProps {
 		instanceId?: string;
@@ -72,13 +98,15 @@ declare module '@wordpress/compose' {
 	type withInstanceId = <P>( WrappedComponent: ComponentType<P> ) => ComponentType<P & withInstanceIdProps> & createHigherOrderComponentProps;
 
 
-	export const createHigherOrderComponent: createHigherOrderComponent;
 	export const useCopyToClipboard: useCopyToClipboard;
 	export const useDebounce: useDebounce;
 	export const withInstanceId: withInstanceId;
 
 	export default interface Compose {
-		createHigherOrderComponent: createHigherOrderComponent;
+		createHigherOrderComponent: typeof createHigherOrderComponent;
+		compose: typeof compose;
+		debounce: typeof debounce;
+		ifCondition: typeof ifCondition;
 		useCopyToClipboard: useCopyToClipboard;
 		useDebounce: useDebounce;
 		withInstanceId: withInstanceId;
