2267a703d1d8176ef0170da9a2e20f1b47a1b859
lipemat <mat@matlipe.com>
2024-07-05 10:00:43 -0400

    Remove expect* which don't actually work.
    Without `tsd` installed, the type assertions are worthless.

--- dev/helpers/assert.d.ts
+++ dev/helpers/assert.d.ts
@@ -1,7 +1,7 @@
 /**
  * Taken from the `tsd` package.
  *
- * We don't need all the functionality of `tsd`, just the type assertion helpers.
+ * We don't need all the functionality of `tsd`, just some of the type assertion helpers.
  */
 
 /**
@@ -11,41 +11,11 @@
  */
 export declare const expectType: <T>( expression: T ) => void;
 /**
- * Asserts that the type of `expression` is not identical to type `T`.
- *
- * @param expression - Expression that should not be identical to type `T`.
- */
-export declare const expectNotType: <T>( expression: any ) => void;
-/**
  * Asserts that the type of `expression` is assignable to type `T`.
  *
  * @param expression - Expression that should be assignable to type `T`.
  */
 export declare const expectAssignable: <T>( expression: T ) => void;
-/**
- * Asserts that the type of `expression` is not assignable to type `T`.
- *
- * @param expression - Expression that should not be assignable to type `T`.
- */
-export declare const expectNotAssignable: <T>( expression: any ) => void;
-/**
- * Asserts that `expression` throws an error. Will not ignore syntax errors.
- *
- * @param expression - Expression that should throw an error.
- */
-export declare const expectError: <T = any>( expression: T ) => void;
-/**
- * Asserts that `expression` is marked as `@deprecated`.
- *
- * @param expression - Expression that should be marked as `@deprecated`.
- */
-export declare const expectDeprecated: ( expression: any ) => void;
-/**
- * Asserts that `expression` is not marked as `@deprecated`.
- *
- * @param expression - Expression that should not be marked as `@deprecated`.
- */
-export declare const expectNotDeprecated: ( expression: any ) => void;
 /**
  * Asserts that the type and return type of `expression` is `never`.
  *
@@ -54,15 +24,3 @@ export declare const expectNotDeprecated: ( expression: any ) => void;
  * @param expression - Expression that should be `never`.
  */
 export declare const expectNever: ( expression: never ) => never;
-/**
- * Prints the type of `expression` as a warning.
- *
- * @param expression - Expression whose type should be printed as a warning.
- */
-export declare const printType: ( expression: any ) => void;
-/**
- * Asserts that the documentation comment of `expression` includes string literal type `T`.
- *
- * @param expression - Expression whose documentation comment should include string literal type `T`.
- */
-export declare const expectDocCommentIncludes: <T>( expression: any ) => void;
