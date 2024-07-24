3b1e1bff2d37aa1593b631b75e5a68d5f6f45750
lipemat <mat@matlipe.com>
2024-07-24 17:08:50 -0400

    Introduce /templates REST API definitions

--- index.d.ts
+++ index.d.ts
@@ -19,6 +19,7 @@ import './wordpress__api/Posts';
 import './wordpress__api/Search';
 import './wordpress__api/Settings';
 import './wordpress__api/Taxonomies';
+import './wordpress__api/Templates';
 import './wordpress__api/Types';
 import './wordpress__api/Users';
 import './wordpress__api/index';
