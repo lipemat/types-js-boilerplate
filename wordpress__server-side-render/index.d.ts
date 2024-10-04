281078cc4965f9c4d6910bc8a6f99927474938af
lipemat <mat@matlipe.com>
2024-10-04 10:48:14 -0400

    Rename ServerSideRender interface to prevent name conflicts

--- wordpress__server-side-render/index.d.ts
+++ wordpress__server-side-render/index.d.ts
@@ -4,31 +4,28 @@
  * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-server-side-render/
  */
 declare module '@wordpress/server-side-render' {
-	import {ComponentType} from '@lipemat/js-boilerplate/helpers';
-	import {PropsWithChildren, ReactElement, ReactNode} from 'react';
+	import type {ComponentType} from '@lipemat/js-boilerplate/helpers';
+	import type {PropsWithChildren, ReactElement, ReactNode} from 'react';
 
 	/**
 	 * @link https://developer.wordpress.org/block-editor/reference-guides/packages/packages-server-side-render/#props
 	 */
-	interface ServerSideRender<A, U> {
+	interface ServerSideRenderProps<A, U> {
 		block: string;
 		attributes?: A;
 		className?: string;
 		httpMethod?: 'GET' | 'POST';
 		urlQueryArgs?: U;
-		EmptyResponsePlaceholder?: ComponentType<ServerSideRender<A, U>>;
-		ErrorResponsePlaceholder?: ComponentType<ServerSideRender<A, U> & {
+		EmptyResponsePlaceholder?: ComponentType<ServerSideRenderProps<A, U>>;
+		ErrorResponsePlaceholder?: ComponentType<ServerSideRenderProps<A, U> & {
 			response: object;
 		}>;
-		LoadingResponsePlaceholder?: ComponentType<ServerSideRender<A, U> & {
+		LoadingResponsePlaceholder?: ComponentType<ServerSideRenderProps<A, U> & {
 			showLoader: boolean;
 			children: ReactNode | ReactNode[];
 		}>;
 		skipBlockSupportAttributes?: boolean;
 	}
 
-
-	function ServerSideRender<A, U = object>( props: PropsWithChildren<ServerSideRender<A, U>>, context?: any ): ReactElement;
-
-	export default ServerSideRender;
+	export default function ServerSideRender<A, U = object>( props: PropsWithChildren<ServerSideRenderProps<A, U>>, context?: any ): ReactElement;
 }
