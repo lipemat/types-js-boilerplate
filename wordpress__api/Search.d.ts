021933b247de7e5d009cb3c15e03a39a20e37329
lipemat <mat@matlipe.com>
2026-03-06 15:20:05 -0500

    Fix SearchQuery REST API arguments
    - Allow passing an array.
    - Remove invalid extension of SearchItem.

--- wordpress__api/Search.d.ts
+++ wordpress__api/Search.d.ts
@@ -4,7 +4,7 @@
  * @link https://developer.wordpress.org/rest-api/reference/search-results/
  */
 declare module '@wordpress/api/search' {
-	import {Category, Collection, Global, Links, Post} from '@wordpress/api';
+	import {Category, Collection, Links, Post} from '@wordpress/api';
 
 	export type SubType = 'post' | 'page' | 'category' | 'post_tag' | 'any';
 
@@ -48,21 +48,14 @@ declare module '@wordpress/api/search' {
 		};
 	}
 
-	/**
-	 * Search Endpoint Query Parameters
-	 *
-	 * @link https://developer.wordpress.org/rest-api/reference/search-results/#list-search-results
-	 *
-	 * @see https://make.wordpress.org/core/2022/10/11/miscellaneous-rest-api-improvements-in-wordpress-6-1/
-	 */
-	export interface SearchQuery<ST extends string = SubType> extends Omit<Global<SearchItem>, '_embed' | 'context'> {
+	export interface SearchQuery<ST extends string = SubType> {
 		context?: 'view' | 'embed';
 		exclude?: number[];
 		include?: number[];
 		page?: number;
 		per_page?: number;
 		search?: string;
-		subtype?: ST;
+		subtype?: ST | ST[];
 		type?: 'post' | 'term' | 'post-format';
 		_embed?: 'self' | true;
 	}
